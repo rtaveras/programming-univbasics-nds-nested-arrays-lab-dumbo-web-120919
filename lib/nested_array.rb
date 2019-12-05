4297e5da447eaf4eefb09fb27dbd57d6ade08504
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 08:15:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -24,9 +24,7 @@ ENFIELD_TENNIS_ACADEMY,
 end
 
 def array_literal_matrix
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
-  # alphabetically by the first character.
+  ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort
 end
 
 def matrix_lookup(matrix, row, column)
